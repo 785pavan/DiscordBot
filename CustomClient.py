cb05c6f6ecdd7b0a223c9aec20f08c671ceefd2a
Pavan Kumar Kandapagari <785pavan@gmail.com>
2021-05-22 18:12:54 +0200

    Create CustomClient.py

---
+++ CustomClient.py
@@ -0,0 +1,16 @@
+import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+
+class CustomClient(discord.Client):
+    async def on_ready(self):
+        print(f'{self.user} has connected to Discord!')
+
+
+client = CustomClient()
+client.run(TOKEN)
